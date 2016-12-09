3e90e88764f996c1e446d918c0946ce8fef2a82d
slavizdravkov <slavizdravkov@gmail.com>
2016-12-09 21:15:08 +0200

    Add functionality: deleteArticle

--- src/SoftUniBlogBundle/Controller/ArticleController.php
+++ src/SoftUniBlogBundle/Controller/ArticleController.php
@@ -84,4 +84,35 @@ class ArticleController extends Controller
             array('article' => $article,
             'form' => $form->createView()));
     }
+
+    /**
+     * @Route("article/delete/{id}", name="article_delete")
+     * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
+     *
+     * @param $id
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function deleteArticle($id, Request $request)
+    {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        if ($article === null){
+            return $this->redirectToRoute("blog_index");
+        }
+
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($article);
+            $em->flush();
+
+            return $this->redirectToRoute('blog_index');
+        }
+
+        return $this->render('article/delete.html.twig', array('article' => $article, 'form' => $form->createView()));
+    }
 }
