875b08ebe055f45acd3140e2058a893a24c8d9e7
Daniela <Litababy@abv.bg>
2016-12-10 13:28:30 +0200

    Listing All Categories on the Home Page

--- src/SoftUniBlogBundle/Controller/HomeController.php
+++ src/SoftUniBlogBundle/Controller/HomeController.php
@@ -7,7 +7,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use SoftUniBlogBundle\Entity\Article;
+use SoftUniBlogBundle\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends Controller
 {
@@ -17,8 +19,19 @@ class HomeController extends Controller
      */
     public function indexAction()
     {
-        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
-
-        return $this->render('blog/index.html.twig', ['articles' => $articles]);
+        $categories = $this->getDoctrine()->getRepository(Category::class)->findAll();
+        return $this->render('blog/index.html.twig', ['categories' => $categories]);
+    }
+    /**
+     * @Route("/category/{id}", name="category_articles")
+     * @param $id
+     *
+     *@return Response
+     */
+    public function listArticles($id)
+    {
+        $category = $this->getDoctrine()->getRepository(Category::class)->find($id);
+        $articles = $category->getArticles()->toArray();
+        return $this->render('article/list.html.twig', ['articles' => $articles, 'category' => $category]);
     }
 }
