8c4f72e9860440ae6781c90f276c6ae7ed067528
slavizdravkov <slavizdravkov@gmail.com>
2016-12-14 22:12:14 +0200

    Change Ïndex View"

--- src/SoftUniBlogBundle/Controller/HomeController.php
+++ src/SoftUniBlogBundle/Controller/HomeController.php
@@ -20,26 +20,8 @@ class HomeController extends Controller
     public function indexAction()
     {
         $categories = $this->getDoctrine()->getRepository(Category::class)->findAll();
-        /** $articles $articles = $this->getDoctrine()->getRepository(Article::class)->findAll(); */
-        $articlesSort = $this->getDoctrine()->getRepository(Article::class)->findBy([], ['count' => 'DESC']);
-        if (count($articlesSort) <= 4){
-            return $this->render('blog/index.html.twig', ['categories' => $categories, 'articles' => $articlesSort]);
-        }
-
-        else{
-            $countArticles = 0;
-            $trimArray = array();
-            foreach ($articlesSort as $articl){
-                if ($countArticles < 4){
-                    $trimArray[$countArticles] = $articl;
-                }
-                $countArticles++;
-            }
-            return $this->render('blog/index.html.twig', ['categories' => $categories, 'articles' => $trimArray]);
-        }
-        /** echo var_dump($articles); */
-        /** echo var_dump($articlesSort); */
-        return $this->render('blog/index.html.twig', ['categories' => $categories, 'articles' => $articlesSort]);
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
+        return $this->render('blog/index.html.twig', ['categories' => $categories, 'articles' => $articles]);
     }
     /**
      * @Route("/category/{id}", name="category_articles")
